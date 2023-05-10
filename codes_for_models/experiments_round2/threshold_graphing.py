805489e56452b812384b92fa6dd5f35e17ed8d2e
CharlesAtUW <chazi@cs.washington.edu>
2023-05-09 20:58:15 -0700

    plotting now saves images

--- codes_for_models/experiments_round2/threshold_graphing.py
+++ codes_for_models/experiments_round2/threshold_graphing.py
@@ -4,7 +4,11 @@ import json
 
 EVAL_DETAILS_FILENAME = "all_evaluation_details.json"
 
-def graph_precision_recall_curve(metrics: pd.DataFrame, additional_points: list, results_name: str):
+def graph_precision_recall_curve(eval_details: dict, metrics: pd.DataFrame, show: bool = False):
+    additional_points = eval_details.get("additional_points", [])
+    results_name = eval_details["name"]
+    save_path = eval_details["plot_save_path"]
+
     thresholds = metrics["threshold"].to_numpy()
     precisions = metrics["precision"].to_numpy()
     recalls = metrics["recall"].to_numpy()
@@ -13,19 +17,22 @@ def graph_precision_recall_curve(metrics: pd.DataFrame, additional_points: list,
 
     plt.figure()
 
-    main_points = plt.scatter(precisions, recalls)
+    main_points = plt.scatter(recalls, precisions)
     for i, th in enumerate(thresholds):
         if i % 10 == 0:
-            plt.annotate(f"{th:.2f}", (precisions[i], recalls[i]))
+            plt.annotate(f"{th:.2f}", (recalls[i], precisions[i]))
 
-    additional_points = plt.scatter(additional_precisions, additional_recalls)
+    additional_points = plt.scatter(additional_recalls, additional_precisions)
 
-    plt.xlabel("Precision")
-    plt.ylabel("Recall")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
     plt.legend((main_points, additional_points), ("Model's metrics", "Paper's metrics"))
     plt.suptitle(f"Precision vs. recall (for various thresholds)", fontsize=18)
     plt.title(results_name, fontsize=10)
-    plt.show()
+    
+    if show:
+        plt.show()
+    plt.savefig(save_path)
 
 
 def main():
@@ -35,7 +42,7 @@ def main():
 
     for ed in all_eval_details:
         metrics = pd.read_csv(ed["metrics_save_path"])
-        graph_precision_recall_curve(metrics, ed["additional_points"], ed["name"])
+        graph_precision_recall_curve(ed, metrics)
 
 
 if __name__ == "__main__":
